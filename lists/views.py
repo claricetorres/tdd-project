6a66a6c0766370e45bb7dae73aba9c712985aac2
Clarice <pawlowcla@yahoo.com.br>
2021-02-24 22:44:00 -0300

    Escrevendo código da aplicação

--- lists/views.py
+++ lists/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home_page():
     pass
+
+#dei um pull acidental e sobreescrevi alguns arquivos (o manage.py ainda não estava comitado pois eu estava usando o git add para cada arquivo) por isso tive que fazer um esforço para fazer o projeto funcionar novamente e os commits ficaram fora de ordem 
