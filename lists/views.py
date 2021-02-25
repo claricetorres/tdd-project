149473893aabf03e71a922e31832ddf8f50d3238
Clarice <pawlowcla@yahoo.com.br>
2021-02-24 23:04:00 -0300

    Ciclo: Teste de Unidade/Desenvolvimento

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_page():
-    pass
-
-#dei um pull acidental e sobreescrevi alguns arquivos (o manage.py ainda não estava comitado pois eu estava usando o git add para cada arquivo) por isso tive que fazer um esforço para fazer o projeto funcionar novamente e os commits ficaram fora de ordem 
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
