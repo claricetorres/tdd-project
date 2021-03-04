54f1db1a82cf5759b7595876729ee43fd2b25bfc
Clarice <pawlowcla@yahoo.com.br>
2021-03-03 23:16:56 -0300

    Entrada inválida não deve ser salva no BD

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.utils.html import escape
 
 class HomePageTest(TestCase):
 
@@ -55,6 +56,11 @@ class NewListTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
         expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
+        
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
     
 class NewItemTest(TestCase):
 
